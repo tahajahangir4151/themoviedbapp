ce79d348f0caf19c9d8d02d8f20c8b8c1fcccb21
tahajahangir4151 <139686825+tahajahangir4151@users.noreply.github.com>
2023-08-22 09:53:08 +0500

    Make Resposnive

--- src/components/TrendingBtn.jsx
+++ src/components/TrendingBtn.jsx
@@ -1,20 +1,41 @@
 import React from "react";
-import { Button, ButtonGroup, Typography, Box } from "@mui/material";
+import {
+  Button,
+  ButtonGroup,
+  Typography,
+  Box,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 
 const TrendingBtn = ({ activeButton, handleButtonClick }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   return (
     <Box
       display="flex"
-      alignItems="center"
+      flexDirection={isMobile ? "column" : "row"}
+      alignItems={isMobile ? "center" : "flex-start"}
       sx={{ marginTop: "30px", marginLeft: "10px" }}
     >
-      <Typography variant="h5" sx={{ marginRight: "10px", marginLeft: "17px" }}>
+      <Typography
+        variant="h5"
+        sx={{
+          marginRight: isMobile ? 0 : "10px",
+          marginLeft: isMobile ? 0 : "17px",
+          marginBottom: isMobile ? "10px" : 0,
+        }}
+      >
         Trending
       </Typography>
       <ButtonGroup
         color="primary"
         aria-label="Trending Timeframe"
-        sx={{ borderRadius: "30px", overflow: "hidden", height: "35px" }}
+        sx={{
+          borderRadius: "30px",
+          overflow: "hidden",
+          height: isMobile ? "auto" : "35px",
+        }}
       >
         <Button
           onClick={() => handleButtonClick("today")}
@@ -24,8 +45,8 @@ const TrendingBtn = ({ activeButton, handleButtonClick }) => {
             fontWeight: "600",
             borderRadius: "30px",
             "&:first-of-type": {
-              borderTopRightRadius: "0",
-              borderBottomRightRadius: "0",
+              borderTopRightRadius: isMobile ? "30px" : "0",
+              borderBottomRightRadius: isMobile ? "30px" : "0",
             },
           }}
         >
@@ -39,8 +60,8 @@ const TrendingBtn = ({ activeButton, handleButtonClick }) => {
             fontWeight: "600",
             borderRadius: "30px",
             "&:last-of-type": {
-              borderTopLeftRadius: "0",
-              borderBottomLeftRadius: "0",
+              borderTopLeftRadius: isMobile ? "30px" : "0",
+              borderBottomLeftRadius: isMobile ? "30px" : "0",
             },
           }}
         >
